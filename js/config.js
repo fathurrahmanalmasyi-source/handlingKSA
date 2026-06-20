7866e95a911b021d9eaf8fcad9adff5b293626ce
fathurrahmanalmasyi-source <fathurrahmanalmasyi@gmail.com>
2026-06-21 00:32:32 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // GANTI URL INI DENGAN WEB APP URL DARI GOOGLE APPS SCRIPT ANDA
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbzeRXTFAw1hQX-Nd0szHCAAKeQBSv7F-hNgk-qlfnZRl8BId1dXI_2Kc68jhT31d52e/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbxyC61vHh7j6Syev9gHcBy-IR4UV5Rh8xAVnZcumd332Mj4O3zAPUGS6GJoFOBgs4sY/exec"
 };
